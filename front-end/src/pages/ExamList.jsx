f7c65a68cd49ced614e8944c47f33d056a2df41a
Divanshu-Singla <149762364+Divanshu-Singla@users.noreply.github.com>
2025-12-21 22:28:57 +0530

    fix 2

--- front-end/src/pages/ExamList.jsx
+++ front-end/src/pages/ExamList.jsx
@@ -55,19 +55,6 @@ const ExamList = () => {
     setCurrentPage(pageNumber);
   };
 
-  if (status === "loading") {
-    return (
-      <div className="flex items-center justify-center h-screen bg-gray-50">
-        <motion.div
-          className="w-16 h-16 border-4 border-blue-600 border-t-transparent rounded-full animate-spin"
-          initial={{ scale: 0 }}
-          animate={{ scale: 1, rotate: 360 }}
-          transition={{ duration: 0.8, ease: "easeInOut", repeat: Infinity }}
-        />
-      </div>
-    );
-  }
-
   if (error) {
     return (
       <div className="flex items-center justify-center h-screen">
